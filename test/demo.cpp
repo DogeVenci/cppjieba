d5b583f906c97415d76d1fb80190009e83f3833b
dovenci <dovenci@gmail.com>
2021-08-29 19:21:17 +0800

    update

--- test/demo.cpp
+++ test/demo.cpp
@@ -2,18 +2,19 @@
 
 using namespace std;
 
-const char* const DICT_PATH = "../dict/jieba.dict.utf8";
-const char* const HMM_PATH = "../dict/hmm_model.utf8";
-const char* const USER_DICT_PATH = "../dict/user.dict.utf8";
-const char* const IDF_PATH = "../dict/idf.utf8";
-const char* const STOP_WORD_PATH = "../dict/stop_words.utf8";
+const char *const DICT_PATH = "../dict/jieba.dict.utf8";
+const char *const HMM_PATH = "../dict/hmm_model.utf8";
+const char *const USER_DICT_PATH = "../dict/user.dict.utf8";
+const char *const IDF_PATH = "../dict/idf.utf8";
+const char *const STOP_WORD_PATH = "../dict/stop_words.utf8";
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
   cppjieba::Jieba jieba(DICT_PATH,
-        HMM_PATH,
-        USER_DICT_PATH,
-        IDF_PATH,
-        STOP_WORD_PATH);
+                        HMM_PATH,
+                        USER_DICT_PATH,
+                        IDF_PATH,
+                        STOP_WORD_PATH);
   vector<string> words;
   vector<cppjieba::Word> jiebawords;
   string s;
@@ -55,16 +56,17 @@ int main(int argc, char** argv) {
   cout << "[demo] Lookup Tag for Single Token" << endl;
   const int DemoTokenMaxLen = 32;
   char DemoTokens[][DemoTokenMaxLen] = {"拖拉机", "CEO", "123", "。"};
-  vector<pair<string, string> > LookupTagres(sizeof(DemoTokens) / DemoTokenMaxLen);
-  vector<pair<string, string> >::iterator it;
-  for (it = LookupTagres.begin(); it != LookupTagres.end(); it++) {
-	it->first = DemoTokens[it - LookupTagres.begin()];
-	it->second = jieba.LookupTag(it->first);
+  vector<pair<string, string>> LookupTagres(sizeof(DemoTokens) / DemoTokenMaxLen);
+  vector<pair<string, string>>::iterator it;
+  for (it = LookupTagres.begin(); it != LookupTagres.end(); it++)
+  {
+    it->first = DemoTokens[it - LookupTagres.begin()];
+    it->second = jieba.LookupTag(it->first);
   }
   cout << LookupTagres << endl;
 
   cout << "[demo] Tagging" << endl;
-  vector<pair<string, string> > tagres;
+  vector<pair<string, string>> tagres;
   s = "我是拖拉机学院手扶拖拉机专业的。不用多久，我就会升职加薪，当上CEO，走上人生巅峰。";
   jieba.Tag(s, tagres);
   cout << s << endl;
---
+++ wasm/jieba.cpp
@@ -0,0 +1,58 @@
+#include "cppjieba/Jieba.hpp"
+
+#ifndef EM_PORT_API
+#if defined(__EMSCRIPTEN__)
+#include <emscripten.h>
+#include <emscripten/bind.h>
+#if defined(__cplusplus)
+#define EM_PORT_API(rettype) extern "C" rettype EMSCRIPTEN_KEEPALIVE
+#else
+#define EM_PORT_API(rettype) rettype EMSCRIPTEN_KEEPALIVE
+#endif
+#else
+#if defined(__cplusplus)
+#define EM_PORT_API(rettype) extern "C" rettype
+#else
+#define EM_PORT_API(rettype) rettype
+#endif
+#endif
+#endif
+
+using namespace std;
+using namespace cppjieba;
+using namespace emscripten;
+
+const char *const DICT_PATH = "../dict/jieba.dict.utf8";
+const char *const HMM_PATH = "../dict/hmm_model.utf8";
+const char *const USER_DICT_PATH = "../dict/user.dict.utf8";
+const char *const IDF_PATH = "../dict/idf.utf8";
+const char *const STOP_WORD_PATH = "../dict/stop_words.utf8";
+
+Jieba jieba(DICT_PATH,
+            HMM_PATH,
+            USER_DICT_PATH,
+            IDF_PATH,
+            STOP_WORD_PATH);
+
+// EM_PORT_API(vector<KeywordExtractor::Word>)
+vector<string> Extract(string s, size_t topN)
+{
+    clock_t start = clock();
+    // vector<KeywordExtractor::Word> keywordres;
+    vector<string> words;
+    jieba.extractor.Extract(s, words, topN);
+    // for (auto keyword : keywordres)
+    // {
+    //     words.push_back(keyword.word);
+    // }
+    clock_t finish = clock();
+    double cost = (double)(finish - start) / CLOCKS_PER_SEC;
+    cout << "onExtract " << cost << endl;
+    return words;
+}
+
+EMSCRIPTEN_BINDINGS(my_module)
+{
+    emscripten::function("Extract", &Extract);
+    register_vector<string>("vector<string>");
+}
